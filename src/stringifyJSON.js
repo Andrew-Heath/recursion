903768eba78644a33a4509f14f510a4fa43a2e4d
andrew-heath <aheath42@gmail.com>
2016-05-06 18:25:43 -0700

    Written loose pseudocode outline

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,8 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  // discern type of value passed in
+  // figure out keys
+  // figure out values attached to keys
+  // create some order to return the values as
 };
