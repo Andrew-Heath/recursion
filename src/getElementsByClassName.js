50cb388a878bd1c910dfc775fc769e5071585b01
andrew-heath <aheath42@gmail.com>
2016-05-06 22:45:55 -0700

    setup get ele by class name, a base case

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  // Set body to what to recurse through
+  if (arguments[1] === undefined) {
+    var body = document.body;
+  } else {
+    var body = arguments[1];
+  }
+
+  // Base Case
+  // Empty Body
+  if (body === []) {
+    return;
+  }
+
+  // Recurse Case
+  var i = arguments[2];
+  
+
 };
